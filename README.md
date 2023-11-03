93f1d77a3f81d7c610e6a3c968018ed9559aedce
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-03 10:44:23 +0000

    prepare for deployment

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # count-api
 
-> This simple Count API is created to count the page loads of some of my Frontend Applications to avoid overcharges.
+> This lightweight API is created to count the page loads of some of my Frontend Applications to be able to avoid overcharges.
 
 Written in Python, using the FastAPI.
 
@@ -8,9 +8,9 @@ Written in Python, using the FastAPI.
 
 ## Functionality
 
-The API identifies the URLs where the request is coming from and if they come from from my approved sites, increments the count by one and returns the counter's state alongside a 200 status response. Otherwise it returns a status of 400, Bad Request.
+The API identifies the requests' URLs and if they are my approved sites, increments the count by one and returns the counter's state alongside a 200 status response. Otherwise it returns a status of 400, Bad Request.
 
-The counter registers and always updates the last visit's date and resets the counter to 1 if it recognises a new month. This is to ensure that the monthly allowances of the counts will reset for a new month.
+The counter always updates the last visit's date and resets the counter to 1 if it recognises a new month. This is to ensure that the monthly allowances of the counts will reset if a new month is detected.
 
 ---
 
@@ -23,11 +23,11 @@ The counter registers and always updates the last visit's date and resets the co
 ## Local Development
 
 0. Make sure python is installed.
-1. create a virtual environment with `python3 -m venv venv`
-2. activate the virtual environment: `source venv/bin/activate`
-3. install the required packages: `pip install -r requirements.txt`
+1. Create a virtual environment with `python3 -m venv venv`
+2. Activate the virtual environment: `source venv/bin/activate`
+3. Install the required packages: `pip install -r requirements.txt`
 
-To run the server on a local development environment:
+To run the server on a local development server:
 
 `uvicorn page_visits_app:app --reload`
 
@@ -35,4 +35,4 @@ To run the server on a local development environment:
 
 ## Deployment
 
-The project is deployed on `AZURE`'s FaaS platform. The postgreSQL database is deployed on ElephantSQL.
+The project is deployed on `Heroku`, the postgreSQL database is deployed on `ElephantSQL`.
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, HTMLResponse
-from datetime import date  # Import date for handling dates
+from datetime import date
 import databases
 import os
 from dotenv import load_dotenv
@@ -19,7 +19,8 @@ async def increment_page_visit(page_name: str, request: Request):
     # Check if the request URL matches the specified page_name
     if request_url == page_name:
         # Query to fetch the count and last_visit for the specified page_name
-        query = "SELECT count, last_visit FROM page_visits WHERE page_name = :page_name"
+        query = "SELECT count, last_visit FROM page_visits \
+                WHERE page_name = :page_name"
         values = {"page_name": page_name}
 
         # Execute the query
@@ -31,23 +32,37 @@ async def increment_page_visit(page_name: str, request: Request):
             current_month = current_date.month
 
             if last_visit_month != current_month:
-                # If the last visit was in a different month, reset the count to zero
-                query = "UPDATE page_visits SET count = 0, last_visit = :current_date WHERE page_name = :page_name RETURNING count, last_visit"
+                # If the last visit was in a different month, reset the count
+                query = "UPDATE page_visits SET count = 1, \
+                    last_visit = :current_date \
+                    WHERE page_name = :page_name RETURNING count, last_visit"
             else:
                 # Otherwise, increment the count and update the last_visit
-                query = "UPDATE page_visits SET count = count + 1, last_visit = :current_date WHERE page_name = :page_name RETURNING count, last_visit"
+                query = "UPDATE page_visits \
+                    SET count = count + 1, \
+                    last_visit = :current_date \
+                    WHERE page_name = :page_name RETURNING count, last_visit"
 
             # Update the count and last_visit in the database
             values = {"page_name": page_name, "current_date": current_date}
-            updated_result = await database.fetch_one(query=query, values=values)
+            updated_result = await database.fetch_one(query=query,
+                                                      values=values)
 
             if updated_result:
                 updated_count = updated_result['count']
                 last_visit = updated_result['last_visit']
-                return JSONResponse(content={"count": updated_count, "last_visit": last_visit}, status_code=200)
+                return JSONResponse(content={"count": updated_count,
+                                             "last_visit": last_visit},
+                                    status_code=200)
             else:
-                return JSONResponse(content={"message": "Page not found in database"}, status_code=404)
+                return JSONResponse(
+                    content={"message": "Page not found in database"},
+                    status_code=404)
         else:
-            return JSONResponse(content={"message": "Page not found in database"}, status_code=404)
+            return JSONResponse(
+                content={"message": "Page not found in database"},
+                status_code=404)
     else:
-        return HTMLResponse(content="Request URL does not match the specified page_name", status_code=400)
+        return HTMLResponse(
+            content="Request URL does not match the specified page_name",
+            status_code=400)
